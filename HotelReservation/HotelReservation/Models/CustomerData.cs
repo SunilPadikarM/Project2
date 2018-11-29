1b26a5a2c52141d0f49c643e64593a82c2e67a49
SunilPadikarM <43018114+SunilPadikarM@users.noreply.github.com>
2018-11-28 23:51:36 -0400

    Login and Signup done

---
+++ HotelReservation/HotelReservation/Models/CustomerData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace HotelReservation.Models
+{
+    public class CustomerData : DbContext
+    {
+        public DbSet<CustomerAccount> customeraccount { get; set; }
+    }
+}
---
+++ Rooms_Create.sql
@@ -0,0 +1,24 @@
+USE [HotelDB]
+GO
+
+/****** Object:  Table [dbo].[Rooms]    Script Date: 2018-11-27 7:14:49 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Rooms](
+	[RoomNumber] [bigint] IDENTITY(1,1) NOT NULL,
+	[RoomType] [varchar](50) NOT NULL,
+	[Price] [float] NULL,
+ CONSTRAINT [PK_Rooms] PRIMARY KEY CLUSTERED 
+(
+	[RoomNumber] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Rooms] SET (LOCK_ESCALATION = DISABLE)
+GO
+
