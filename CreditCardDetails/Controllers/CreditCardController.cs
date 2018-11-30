8c865a1a130126e39abc516866d7fe943e6bc85a
kannappan <Kothai.Kannappan.Murugappan@smu.ca>
2018-11-30 17:54:09 -0400

    first commit with basic view

---
+++ CreditCardDetails/Controllers/CreditCardController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CreditCardDetails.Models;
+namespace CreditCardDetails.Controllers
+{
+    public class CreditCardController : Controller
+    {
+        // GET: CreditCard
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreditCardInformation()
+        {
+            return View();
+        }
+
+       /* public string CheckRadio(FormCollection f)
+        {
+            string card = f["CardType"].ToString();
+            
+        }*/
+
+  
+        
+      /*  [HttpPost]
+        public ActionResult CreditCardInformation(CardDetails cardDetails)
+        {
+            if (ModelState.IsValid)
+            {
+               
+            }
+
+            return View();
+        }
+        */
+    }
+}
