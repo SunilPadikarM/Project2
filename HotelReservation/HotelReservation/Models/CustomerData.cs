a05750dfa12a508d1ad92af6311653b00fb09eb6
A00430095 <43040292+SarbottamThapaMagar@users.noreply.github.com>
2018-12-03 18:18:59 -0400

    final commit

--- HotelReservation/HotelReservation/Models/CustomerData.cs
+++ HotelReservation/HotelReservation/Models/CustomerData.cs
@@ -3,32 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 
 namespace HotelReservation.Models
 {
     public class CustomerData : DbContext
     {
-
-
-        public DbSet<CustomerAccount> customeraccount { get; set; }
-
-    
-    }
-
-
-    
-}
-
         public CustomerData()
            : base("name=CustomerDetails1")
         {
         }
         public DbSet<CustomerAccount> customeraccount { get; set; }
 
-        //public DbSet<Customer> Customers { get; set; }
+        public DbSet<CardDetail> cardDetails { get; set; }
 
         public DbSet<Reservation> reservations { get; set; }
 
